90aac58b9585d99512baf08a614a3c323237e738
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-08-17 22:22:02 +0530

    basic setup done

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+
+const server = http.createServer(app);
+
+const socketio = require("socket.io");
+const io = socketio(server, {
+    cors : {
+        origin : "*"
+    }
+});
+
+
+
+
+
+io.on("connection", (socket) => {
+    console.log("New Web-Socket Connection");
+
+    socket.on("message", (message) => {
+    })
+
+    socket.on("message", (payload) => {
+        console.log("Payload : ", payload);
+        io.emit("message", payload);
+    })
+})
+
+
+
+
+
+server.listen(5000 , () => {
+    console.log("Server is listening at port 5000");
+})
+
